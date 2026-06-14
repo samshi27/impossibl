0b881af9611df650506d12eafb5bb49384a8a377
Samshitha <samshi.c@gmail.com>
2026-06-14 23:38:46 +0530

    (env): update api base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'impossibl-api-production.up.railway.app/api',
+  apiBaseUrl: 'https://impossibl-api-production.up.railway.app/api',
 };
