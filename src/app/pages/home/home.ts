f4fccfb0fb104bbf66e5a8a34d1a48a664c1332d
Samshitha <samshi.c@gmail.com>
2026-06-14 23:56:48 +0530

    (hotfix): notify author to write their first post

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { PostService } from '../../services/post';
 import { PostCard } from '../../components/post-card/post-card';
 import { CARD_VARIANTS } from '../../constants/card-variants';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [PostCard],
+  imports: [PostCard, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
